83733bf67b3c21174d05f93b5cbc1dff0a421072
Vishwajeet Parase <37467191+vishwajeetparase@users.noreply.github.com>
2024-08-25 12:56:53 +0000

    reverseString Problem

---
+++ Strings/ReverseStrings.java
@@ -0,0 +1,19 @@
+public class ReverseStrings {
+
+   // public String reverseString(String s){ //Showing error - check why
+   public static String reverseString(String s){
+        String resultString = new String();
+        for(int i = s.length()-1; i>=0; i--){
+            resultString+=s.charAt(i);
+        }
+        return resultString;
+    }
+
+
+    public static void main(String[] args) {
+        String inputString = "vishwajeet";
+        String outputString = reverseString(inputString);
+        System.out.println(outputString);
+    }
+
+}
